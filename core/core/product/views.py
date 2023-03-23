492b1338589d34b2b0cb0588a3b77ad7c94ca620
dr-zieba <zieba1jan@gmail.com>
2023-03-23 21:05:17 +0100

    com-23.03

--- core/core/product/views.py
+++ core/core/product/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework.response import Response
+from rest_framework.decorators import action
 from drf_spectacular.utils import extend_schema
 from .models import Category, Brand, Product
 from .serializers import CategorySerializer, BrandSerializer, ProductSerializer
@@ -32,4 +33,17 @@ class ProductView(viewsets.ViewSet):
     @extend_schema(responses=ProductSerializer)
     def list(self, request):
         serializer = ProductSerializer(self.queryset, many=True)
+        return Response(serializer.data)
+
+    @action(
+        methods=["get"],
+        detail=False,
+        url_path=r"category/(?P<category>\w+)/all",
+        url_name="all",
+    )
+    def list_product_by_category(self, request, category=None):
+        """Viewset of products filtered by category name"""
+        serializer = ProductSerializer(
+            self.queryset.filter(category__name=category), many=True
+        )
         return Response(serializer.data)
